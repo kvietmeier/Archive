60e1045b8a629f5250b42bf4736470e6b57c472c
Karl Vietmeier <karlv@storagenet.org>
2016-01-30 12:58:36 -0800

    Update Files

--- multi-test/scripts/vboxctl.sh
+++ multi-test/scripts/vboxctl.sh
@@ -3,32 +3,48 @@
 #
 # controlvm  <uuid|vmname> <pause|resume|reset|poweroff|savestate>
 
+read -p "What do you want to do? " option
+echo -en "I want to "
+
 case $option in
    pause)
     action=pause
+    echo "$action"
     ;; 
    resume)
     action=resume
+    echo "$action"
     ;;
    reset)
     action=reset
+    echo "$action"
     ;;
    poweroff)
     action=poweroff
+    echo "$action"
     ;;
    savestate)
     action=savestate
+    echo "$action"
     ;;
    start)
-    "run startvm
+    action=start
+    echo "$action"
     ;;
 
 
 
 
 # Start VMs in servers.yml file
-for vm in $(grep name ./servers.yml | awk '{print $3}')
-  do
-    vboxmanage startvm $vm --type headless
-  done
+#for vm in $(grep name ./servers.yml | awk '{print $3}')
+#  do
+#    vboxmanage startvm $vm --type headless
+#  done
+#
+#
+## Start VMs in servers.yml file
+#for vm in $(grep name ./servers.yml | awk '{print $3}')
+#  do
+#    vboxmanage startvm $vm --type headless
+#  done
 
